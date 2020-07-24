7e9c5abeb3d42bc432a3b9851c03e18827822cb7
JeannePreira <jeannepreira95@gmail.com>
2020-07-24 13:41:48 +0100

    fixtureUpdate

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,44 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Profil;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder ){
+
+    $this->encoder=$encoder;
+    }
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        $profils =["ADMIN","FORMATEUR","CM"];
+        foreach ($profils as $key => $libelle) {
+            $profil =new Profil() ;
+            $profil ->setLibelle ($libelle );
+            $manager ->persist($profil);
+            $manager ->flush();
+            
+            for ($i=1; $i <=3 ; $i++) {
+                $user = new User();
+                $user ->setProfil ($profil);
+                $user ->setUsername (strtolower ($libelle ).$i);
+                $user ->setNom($faker->name);
+                $user ->setPrenom($faker->lastName);
+                $user ->setEmail($faker->email);
+                //Génération des Users
+                $password = $this->encoder->encodePassword ($user, 'pass_1234' );
+                $user ->setPassword ($password );
+                $manager ->persist($user);
 
-        $manager->flush();
+            }
+                $manager ->flush();
+        }
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -33,9 +33,7 @@ class User implements UserInterface
      */
     private $username;
 
-    /**
-     * @ORM\Column(type="json")
-     */
+    
     private $roles = [];
 
     /**
@@ -101,7 +99,7 @@ class User implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = 'ROLE_'.$this->profil->getLibelle();
 
         return array_unique($roles);
     }
