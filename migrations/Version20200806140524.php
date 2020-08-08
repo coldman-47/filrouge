6fe455260073cf8f4e79e3e65246d22beec769dc
JeannePreira <jeannepreira95@gmail.com>
2020-08-08 13:52:01 +0000

    jeanne

--- migrations/Version20200806140524.php
+++ migrations/Version20200806140524.php
@@ -10,11 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-<<<<<<< HEAD:migrations/Version20200806140524.php
-final class Version20200806140524 extends AbstractMigration
-=======
 final class Version20200806170931 extends AbstractMigration
->>>>>>> coldman:migrations/Version20200806170931.php
 {
     public function getDescription() : string
     {
@@ -24,20 +20,12 @@ final class Version20200806170931 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-<<<<<<< HEAD:migrations/Version20200806140524.php
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_E6D6B297A4D60759 ON profil (libelle)');
-=======
         $this->addSql('CREATE UNIQUE INDEX UNIQ_2C3959A3A4D60759 ON groupe_competence (libelle)');
->>>>>>> coldman:migrations/Version20200806170931.php
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-<<<<<<< HEAD:migrations/Version20200806140524.php
-        $this->addSql('DROP INDEX UNIQ_E6D6B297A4D60759 ON profil');
-=======
         $this->addSql('DROP INDEX UNIQ_2C3959A3A4D60759 ON groupe_competence');
->>>>>>> coldman:migrations/Version20200806170931.php
     }
 }
