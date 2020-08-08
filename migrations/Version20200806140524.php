0356ccef3f788a4867558661a8c41d84f47a24c8
coldman-47 <ocamara015@gmail.com>
2020-08-08 13:03:05 +0000

    merge update

--- migrations/Version20200806140524.php
+++ migrations/Version20200806140524.php
@@ -10,34 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-<<<<<<< HEAD:migrations/Version20200806140524.php
-final class Version20200806140524 extends AbstractMigration
-=======
 final class Version20200806170931 extends AbstractMigration
->>>>>>> coldman:migrations/Version20200806170931.php
 {
-    public function getDescription() : string
-    {
-        return '';
-    }
+        public function getDescription(): string
+        {
+                return '';
+        }
 
-    public function up(Schema $schema) : void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-<<<<<<< HEAD:migrations/Version20200806140524.php
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_E6D6B297A4D60759 ON profil (libelle)');
-=======
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_2C3959A3A4D60759 ON groupe_competence (libelle)');
->>>>>>> coldman:migrations/Version20200806170931.php
-    }
+        public function up(Schema $schema): void
+        {
+                // this up() migration is auto-generated, please modify it to your needs
+                $this->addSql('CREATE UNIQUE INDEX UNIQ_2C3959A3A4D60759 ON groupe_competence (libelle)');
+        }
 
-    public function down(Schema $schema) : void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-<<<<<<< HEAD:migrations/Version20200806140524.php
-        $this->addSql('DROP INDEX UNIQ_E6D6B297A4D60759 ON profil');
-=======
-        $this->addSql('DROP INDEX UNIQ_2C3959A3A4D60759 ON groupe_competence');
->>>>>>> coldman:migrations/Version20200806170931.php
-    }
+        public function down(Schema $schema): void
+        {
+                // this down() migration is auto-generated, please modify it to your needs
+                $this->addSql('DROP INDEX UNIQ_2C3959A3A4D60759 ON groupe_competence');
+        }
 }
